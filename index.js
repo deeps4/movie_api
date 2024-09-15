972c8609b1ed052bc37a31fd0585f5f9864ce267
deeps4 <deepasharma04@yahoo.com>
2024-09-15 15:26:21 +0200

    Using Connection string for mongo db from environmental variable.

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ let auth = require('./auth')(app);
 
 const { check, validationResult } = require('express-validator');
 
-
-mongoose.connect('mongodb://localhost:27017/cfdb', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
