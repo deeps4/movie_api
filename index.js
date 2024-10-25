046cc2dcacccb38889134cd8178ec79007408315
deeps4 <deepasharma04@yahoo.com>
2024-10-25 17:08:05 +0200

    Remove passport.autheenticate from /movies endpoint.

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 
 let auth = require('./auth')(app);
 
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
     Movies.find()
         .then((movies) => {
             res.status(201).json(movies);
